0e8f949e1055dc64f66d77a80be562e0bad5dc24
Christian Liebel <christian.liebel@thinktecture.com>
2015-10-01 12:14:12 +0200

    Add geolocation service

---
+++ app/app/common/services/geolocation.js
@@ -0,0 +1,29 @@
+!function ($, jQuery, window, document) {
+    "use strict";
+
+    /**
+     * @ngdoc service
+     * @public
+     *
+     * @param $q
+     */
+    function Geolocation($q) {
+        this.getCoordinatesFromSensor = function () {
+            var deferred = $q.defer();
+
+            if ($window.navigator && $window.navigator.geolocation) {
+                $window.navigator.geolocation.getCurrentPosition(function (position) {
+                    deferred.resolve(position.coords);
+                }, function (err) {
+                    deferred.reject(err);
+                })
+            } else {
+                deferred.reject('This platform does not support geolocation.');
+            }
+
+            return deferred.promise;
+        };
+    }
+
+    app.module.service('geolocation', Geolocation);
+}();
