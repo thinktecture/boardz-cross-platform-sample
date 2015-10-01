fd0e699918f9932856afce8b72b75842640c61ee
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-10-01 11:52:46 +0200

    Look at my API, my API will be amazing!

---
+++ api/BoardGame/BoardGame.Api/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+namespace BoardGame.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var httpConfiguration = CreateHttpConfiguration();
+
+            appBuilder.UseWebApi(httpConfiguration);
+        }
+
+        private HttpConfiguration CreateHttpConfiguration()
+        {
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.MapHttpAttributeRoutes();
+            httpConfiguration.Routes.MapHttpRoute("default", "api/{controller}/{action}");
+
+            httpConfiguration.Formatters.Clear();
+            httpConfiguration.Formatters.Add(new JsonMediaTypeFormatter()
+            {
+                SerializerSettings = new JsonSerializerSettings()
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                }
+            });
+            
+            httpConfiguration.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+            return httpConfiguration;
+        }
+    }
+}
---
+++ api/BoardGame/BoardGame.Host/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BoardGame.Api;
+using Microsoft.Owin.Hosting;
+
+namespace BoardGame.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://+:8090"))
+            {
+                Console.WriteLine("Server is up and running");
+                Console.ReadLine();
+            }
+        }
+    }
+}
