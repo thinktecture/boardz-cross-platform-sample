639305426db8d83e9c6c83fe2f5cccf643cdfe4a
Christian Liebel <christian.liebel@thinktecture.com>
2015-10-01 16:19:39 +0200

    Implement radius search

--- app/app/radiusSearch/radiusSearch.js
+++ app/app/radiusSearch/radiusSearch.js
@@ -7,12 +7,34 @@
      *
      * @param $scope
      * @param {Geolocation} geolocation
+     * @param {PlayersApi} playersApi
      */
-    function RadiusSearchController($scope, geolocation) {
-        $scope.locate = function () {
+    function RadiusSearchController($scope, geolocation, playersApi) {
+        $scope.radiuses = [5, 10, 25, 50, 100, 250];
+        $scope.model = {
+            radius: 10
+        };
+
+        init();
+
+        function init() {
             geolocation.getCoordinatesFromSensor()
-                .then(function (coords) {
-                    alert('lat: ' + coords.latitude + ', lng:' + coords.longitude);
+                .then(function (coordinates) {
+                    $scope.coordinates = coordinates;
+                }, function () {
+                    $scope.geolocationUnavailable = true;
+                })
+        }
+
+        $scope.search = function () {
+            playersApi.findNearby($scope.model.radius, {
+                latitude: $scope.coordinates.latitude,
+                longitude: $scope.coordinates.longitude
+            })
+                .then(function (results) {
+                    $scope.results = results;
+                }, function (err) {
+                    // TODO: Err
                 });
         };
     }
