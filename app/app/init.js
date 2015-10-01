1c37e6d9b1a0a377047535c224d8b9fa343aae06
Christian Liebel <christian.liebel@thinktecture.com>
2015-10-01 20:47:52 +0200

    Add angular animate

--- app/app/init.js
+++ app/app/init.js
@@ -2,7 +2,7 @@
 	'use strict';
 	
 	window.app = window.app || {};
-	window.app.module = angular.module('xplatform-sample', ['ui.router', 'pascalprecht.translate', 'ngSanitize', 'ngNotify', 'angular-loading-bar', 'leaflet-directive']);
+	window.app.module = angular.module('xplatform-sample', ['ui.router', 'pascalprecht.translate', 'ngSanitize', 'ngNotify', 'angular-loading-bar', 'leaflet-directive', 'ngAnimate']);
 
 	// insert the base URL here
 	app.module.constant('apiBaseUrl', 'https://boardgameapi.azurewebsites.net/');
