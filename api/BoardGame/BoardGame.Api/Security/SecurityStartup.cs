889b472b354c5955bca91054671b8ee864d74e11
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-10-01 15:53:55 +0200

    Cors for OWIN!

--- api/BoardGame/BoardGame.Api/Security/SecurityStartup.cs
+++ api/BoardGame/BoardGame.Api/Security/SecurityStartup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
 
@@ -10,6 +11,8 @@ namespace BoardGame.Api.Security
     {
         public static void Configuration(IAppBuilder appBuilder)
         {
+            appBuilder.UseCors(CorsOptions.AllowAll);
+                
             appBuilder.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions()
             {
                 TokenEndpointPath = new PathString("/token"),
