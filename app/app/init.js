e42ac9e136e73792ea2a89dc6cfd9af927dc0c69
Christian Liebel <christian.liebel@thinktecture.com>
2015-10-01 12:10:34 +0200

    Un-brand sample

--- app/app/init.js
+++ app/app/init.js
@@ -2,7 +2,7 @@
 	'use strict';
 	
 	window.app = window.app || {};
-	window.app.module = angular.module('basta-sample', ['ui.router', 'pascalprecht.translate', 'ngSanitize']);
+	window.app.module = angular.module('xplatform-sample', ['ui.router', 'pascalprecht.translate', 'ngSanitize']);
 	
     FastClick.attach(document.body);
 	
