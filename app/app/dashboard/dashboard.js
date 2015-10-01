b385d45824900b590906a9156b69781c76b77960
Christian Liebel <christian.liebel@thinktecture.com>
2015-10-01 19:42:21 +0200

    Implemented dashboard view

--- app/app/dashboard/dashboard.js
+++ app/app/dashboard/dashboard.js
@@ -6,9 +6,25 @@
 	 * @constructor
 	 * 
 	 * @param $scope
+	 * @param {PlayersApi} playersApi
+	 * @param {BoardGamesApi} boardGamesApi
 	 */
-	function DashboardController($scope) {
-		
+	function DashboardController($scope, playersApi, boardGamesApi) {
+        $scope.playerCount = $scope.gameCount = '?';
+
+		init();
+
+		function init() {
+			playersApi.list()
+				.then(function (results) {
+					$scope.playerCount = results.length;
+				});
+
+			boardGamesApi.list()
+				.then(function (results) {
+					$scope.gameCount = results.length;
+				});
+		}
 	}
 	
 	app.module.controller('dashboardController', DashboardController);
