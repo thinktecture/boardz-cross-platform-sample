75153bd88259107173b478e0a1572beee8455df9
Christian Liebel <christian.liebel@thinktecture.com>
2015-10-01 15:50:36 +0200

    Game list loads games from API

--- app/app/gameList/gameList.js
+++ app/app/gameList/gameList.js
@@ -6,9 +6,17 @@
 	 * @constructor
 	 * 
 	 * @param $scope
+	 * @param {BoardGamesApi} boardGamesApi
 	 */
-	function GameListController($scope) {
-		
+	function GameListController($scope, boardGamesApi) {
+		init();
+
+        function init() {
+            boardGamesApi.list()
+                .then(function (games) {
+                    $scope.games = games;
+                });
+        }
 	}
 	
 	app.module.controller('gameListController', GameListController);
