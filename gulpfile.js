c3456afa7f72d534b0d59189c73026bb2259243c
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-10-01 10:34:15 +0200

    Build update

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
     sh = require('shelljs');
 
 gulp.task('clean', function (done) {
-    del(['app/cordova/www'])
+    del(['app/cordova/www', 'app/cordova/platforms', 'app/cordova/plugins'])
         .then(function () {
             done();
         });
@@ -23,6 +23,8 @@ gulp.task('build:cordova', ['clean', 'copy-source'], function (done) {
     sh.cd('app/cordova');
     sh.exec('cordova platform add ios');
     sh.exec('cordova platform add android');
+    sh.exec('cordova platform add windows');
+    sh.exec('cordova plugin add org.apache.cordova.statusbar');
     sh.exec('cordova build');
     done();
 });
