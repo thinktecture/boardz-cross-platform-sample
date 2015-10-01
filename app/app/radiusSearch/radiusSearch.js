0e5a48bddea8ccfdc06416b12dbd94a0dc15caed
Christian Liebel <christian.liebel@thinktecture.com>
2015-10-01 12:04:01 +0200

    Add radius search view

---
+++ app/app/radiusSearch/radiusSearch.js
@@ -0,0 +1,15 @@
+!function ($, jQuery, window, document) {
+    'use strict';
+
+    /**
+     * @public
+     * @constructor
+     *
+     * @param $scope
+     */
+    function RadiusSearchController($scope) {
+
+    }
+
+    app.module.controller('radiusSearchController', RadiusSearchController);
+}();
