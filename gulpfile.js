93902613bebb875ea9b91b6e48aa8f442d263fb4
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-10-04 15:18:23 +0200

    Added changes from CW

--- gulpfile.js
+++ gulpfile.js
@@ -43,14 +43,21 @@ var files = {
     ]
 };
 
-gulp.task('clean', function (done) {
-    del(['app/cordova/www', 'app/cordova/platforms', 'app/cordova/plugins', 'app/nwjs/www', 'app/nwjs/build'])
+gulp.task('clean:cordova', function (done) {
+    del(['app/cordova/www', 'app/cordova/platforms', 'app/cordova/plugins'])
         .then(function () {
             done();
         });
 });
 
-gulp.task('copy-source', ['clean'], function () {
+gulp.task('clean:nwjs', function (done) {
+    del(['app/nwjs/www', 'app/nwjs/build'])
+        .then(function () {
+            done();
+        });
+});
+
+gulp.task('copy-source', ['clean:cordova', 'clean:nwjs'], function () {
     return gulp.src([
         'app/**/*.*',
         '!app/cordova/**/*.*',
@@ -60,7 +67,7 @@ gulp.task('copy-source', ['clean'], function () {
         .pipe(gulp.dest('app/nwjs/www'));
 });
 
-gulp.task('build:cordova', ['clean', 'copy-source'], function (done) {
+gulp.task('build:cordova', ['clean:cordova', 'copy-source'], function (done) {
     sh.cd('app/cordova');
     sh.exec('cordova platform add ios');
     sh.exec('cordova platform add android');
@@ -74,7 +81,7 @@ gulp.task('build:cordova', ['clean', 'copy-source'], function (done) {
     done();
 });
 
-gulp.task('build:nwjs', ['clean', 'copy-source'], function () {
+gulp.task('build:nwjs', ['clean:nwjs', 'copy-source'], function () {
     var nw = new NwBuilder({
         version: '0.12.3',
         files: './app/nwjs/**/**',
@@ -100,4 +107,4 @@ gulp.task('index:dev', function () {
         .pipe(gulp.dest('app'));
 });
 
-gulp.task('default', ['clean', 'copy-source', 'build:cordova']);
+//gulp.task('default', ['clean', 'copy-source', 'build:cordova']);
