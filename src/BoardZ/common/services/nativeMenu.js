02469375decc07b0e8fbe6532b4f61df26af14f2
Christian Weyer <christian.weyer@thinktecture.com>
2015-10-11 20:40:17 +0200

    Added native Mac menu for nw.js

---
+++ src/BoardZ/common/services/nativeMenu.js
@@ -0,0 +1,24 @@
+(function ($, jQuery, window, document) {
+    "use strict";
+
+    /**
+     * @param {PlatformInformation} platformInformation
+     * @constructor
+     */
+    function NativeMenu(platformInformation, $log) {
+        if (platformInformation.isNwjs()) {
+            var gui = require('nw.gui');
+
+            var nativeMenuBar = new gui.Menu({ type: 'menubar' });
+
+            if (process.platform === 'darwin') {
+                nativeMenuBar.createMacBuiltin('BoardZ!');
+            }
+
+            var window = gui.Window.get();
+            window.menu = nativeMenuBar;
+        }
+    }
+
+    app.module.service('nativeMenu', NativeMenu);
+})();
