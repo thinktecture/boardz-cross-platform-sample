84beec801368016764bd1a82cd28384c5340f3cb
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2016-01-07 09:54:32 +0100

    Code cleanup

--- src/BoardZApi/BoardGame.Api/Security/IdentityServer/MembershipRebootUserService.cs
+++ src/BoardZApi/BoardGame.Api/Security/IdentityServer/MembershipRebootUserService.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BoardGame.Api.Security.MembershipReboot;
+﻿using BoardGame.Api.Security.MembershipReboot;
 using IdentityServer3.Core.Configuration;
 using IdentityServer3.Core.Services;
 using IdentityServer3.MembershipReboot;
 
 namespace BoardGame.Api.Security.IdentityServer
 {
-
         public static class CustomUserServiceExtensions
         {
             public static void ConfigureCustomUserService(this IdentityServerServiceFactory factory, string connString)
