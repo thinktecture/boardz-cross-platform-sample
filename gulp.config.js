c04305e1cd527148bfe89b555c157cfd3aa16e21
Manuel Rauber <manuel.rauber@thinktecture.com>
2016-01-18 20:52:40 +0100

    Fixes #33 Copy empty cordova.js to prevent 404

--- gulp.config.js
+++ gulp.config.js
@@ -8,6 +8,7 @@ module.exports = {
         files: {
             app: {
                 js: [
+                    'cordova.js',
                     'app/init.js',
                     'app/**/*.js',
                     'common/**/*.js',
