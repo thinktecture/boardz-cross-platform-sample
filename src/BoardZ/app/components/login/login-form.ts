de213e384fca32cef9413d8193354a9565007127
Sebastian P.R. Gingter <sebastian@gingter.org>
2016-02-10 13:50:58 +0100

    Login does now work and look fine

--- src/BoardZ/app/components/login/login-form.ts
+++ src/BoardZ/app/components/login/login-form.ts
@@ -1,18 +1,27 @@
 import {Component} from 'angular2/core';
-import {FORM_DIRECTIVES, FormBuilder, Validators, ControlGroup} from 'angular2/common';
+import {FORM_DIRECTIVES, FormBuilder, Validators, ControlGroup, NgClass} from 'angular2/common';
 import {Router, ROUTER_DIRECTIVES, CanDeactivate, ComponentInstruction} from 'angular2/router';
 
 import {LoginService} from '../../services/login/login-service';
 
 @Component({
     selector: 'login-form',
-    directives: [FORM_DIRECTIVES, ROUTER_DIRECTIVES],
+    directives: [FORM_DIRECTIVES, ROUTER_DIRECTIVES, NgClass],
     templateUrl: 'app/components/login/login-form.html'
 })
 export class LoginForm implements CanDeactivate {
 
     public credentialForm: ControlGroup;
     public loginError: boolean = false;
+    public triedAbort: boolean = false;
+
+    get isUsernameValid(): boolean {
+        return this.credentialForm.controls['username'].valid || this.credentialForm.controls['username'].pristine
+    }
+
+    get isPasswordValid(): boolean {
+        return this.credentialForm.controls['password'].valid || this.credentialForm.controls['password'].pristine
+    }
 
     constructor(private _router: Router, private _loginService: LoginService, formBuilder: FormBuilder) {
         this.credentialForm = formBuilder.group({
@@ -25,6 +34,8 @@ export class LoginForm implements CanDeactivate {
         console.log('Submitting by event: ' + evt.toString())
         evt.preventDefault();
 
+        this.triedAbort = false;
+
         var username = this.credentialForm.controls['username'].value,
             password = this.credentialForm.controls['password'].value;
 
@@ -35,6 +46,12 @@ export class LoginForm implements CanDeactivate {
                 () => { this.setError(false); this._router.navigate(['Dashboard']) },
                 () => { this.setError(true); }
             );
+    }
+
+    tryAbort(evt): void {
+        evt.preventDefault();
+
+        this.triedAbort = true;
     }
 
     setError(value: boolean) {
