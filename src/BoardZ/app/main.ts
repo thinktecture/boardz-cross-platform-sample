173f6ab0f70b73a3e5e304c3301444eb46faf0d3
Sebastian P.R. Gingter <sebastian@gingter.org>
2016-02-11 18:49:48 +0100

    Using different routing strategy to have better reloading

--- src/BoardZ/app/main.ts
+++ src/BoardZ/app/main.ts
@@ -1,6 +1,6 @@
 import {bootstrap} from 'angular2/platform/browser';
-import {ComponentRef} from 'angular2/core';
-import {ROUTER_PROVIDERS} from 'angular2/router';
+import {ComponentRef, provide} from 'angular2/core';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 
 import {BoardzApp} from './boardz-app';
 import {appInjector} from './services/routing/appInjector';
@@ -9,6 +9,7 @@ import {LOGGING_PROVIDERS} from './services/logging/loggingProviders';
 
 bootstrap(BoardzApp, [
     ROUTER_PROVIDERS,            // Bootstrap the initial routing stuff
+    provide(LocationStrategy, { useClass: HashLocationStrategy }), // best for auto-reloading
     LOGGING_PROVIDERS,
     TokenDataStore               // this is also required very early, so we need our dependencies registered first
 ]).then((appRef: ComponentRef) => {
