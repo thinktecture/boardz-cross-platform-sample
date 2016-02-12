f01b0a6891a653c60f8b3f1e32ac52e912284948
Sebastian P.R. Gingter <sebastian@gingter.org>
2016-02-12 12:47:52 +0100

    Prepared switch for angulars prod mode

--- src/BoardZ/app/main.ts
+++ src/BoardZ/app/main.ts
@@ -1,11 +1,13 @@
 import {bootstrap} from 'angular2/platform/browser';
-import {ComponentRef, provide} from 'angular2/core';
+import {ComponentRef, provide, enableProdMode} from 'angular2/core';
 import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 
 import {BoardzApp} from './boardz-app';
 import {appInjector} from './services/routing/appInjector';
 import {LOGGING_PROVIDERS} from './services/logging/loggingProviders';
 import {TokenDataStore} from './services/login/tokenDataStore';
+
+// enableProdMode();  // turns off Angulars 2nd change detection pass and assertions, also disables our diagnostic output
 
 bootstrap(BoardzApp, [
     ROUTER_PROVIDERS,            // Bootstrap the initial routing stuff
