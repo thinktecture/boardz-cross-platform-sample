6809c5a05d91048b7ec1e13ad6ea925eac62b089
Thorsten Hans <thorsten.hans@gmail.com>
2016-02-21 10:45:49 +0100

    added geolocation service

---
+++ src/BoardZ/app/services/geolocation/geolocation.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from 'angular2/core';
+import {GeoLocation} from '../../models/geolocation';
+
+@Injectable()
+export class GeolocationService {
+
+    public locate() {
+        return new Promise((resolve, reject)=> {
+            if (window.navigator && window.navigator.geolocation) {
+                window.navigator.geolocation.getCurrentPosition((pos)=> {
+                    resolve(new GeoLocation(pos.coords.latitude, pos.coords.longitude, 16));
+                }, (err)=> {
+                    reject(err);
+                }, {
+                    enableHighAccuracy: true,
+                    timeout: 5000,
+                    maximumAge: 0
+                });
+            } else {
+                reject('This platform does not support geolocation.');
+            }
+        });
+    }
+} 
