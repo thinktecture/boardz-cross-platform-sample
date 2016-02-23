33ff1517f69406b485248445e178235c4e18fb3d
Thorsten Hans <thorsten.hans@gmail.com>
2016-02-23 18:57:49 +0100

    removed middleware and used decorator instead -> single usage

--- src/BoardZ/app/decorators/needsAuthentication.ts
+++ src/BoardZ/app/decorators/needsAuthentication.ts
@@ -1,6 +1,18 @@
-import {CanActivate} from 'angular2/router';
-import {isLoggedIn} from '../middlewares/isloggedin.middleware';
+import {CanActivate, ComponentInstruction, Router} from 'angular2/router';
+import {Injector} from 'angular2/core';
+import {appInjector} from '../services/app.injector';
+import {TokenDataStore} from '../services/token.service';
 
 export const NeedsAuthentication = () => {
-    return CanActivate(isLoggedIn);
+    return CanActivate((to: ComponentInstruction, from: ComponentInstruction, target = ['/']) => {
+        let injector: Injector = appInjector(); // Get the stored reference to the application injector
+        let tokenStore: TokenDataStore = injector.get(TokenDataStore);
+        let router: Router = injector.get(Router);
+
+        if (tokenStore.token)
+            return true;
+
+        router.navigate(['/Login', { target }]);
+        return false;
+    });
 }
