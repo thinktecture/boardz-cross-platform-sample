1a2d3f4df51f1042d23dfb8e13bf762cf209700f
Thorsten Hans <thorsten.hans@gmail.com>
2016-02-25 23:14:52 +0100

    player and nearbyplayer models

--- src/BoardZ/app/models/player.ts
+++ src/BoardZ/app/models/player.ts
@@ -1,3 +1,11 @@
+import {GeoLocation} from './geolocation';
+
 export class Player{
     public id: string;
+    public name: string;
+    public boardGameId: string;
+    public boardGameName: string;
+    public coordinate: GeoLocation;
+    public imageUrl: string;
+
 }
