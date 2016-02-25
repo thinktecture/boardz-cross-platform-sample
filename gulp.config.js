ab4e39e2834a6b482207746fde457b890edcda8c
Thorsten Hans <thorsten.hans@gmail.com>
2016-02-26 00:23:10 +0100

    cordova path fixed

--- gulp.config.js
+++ gulp.config.js
@@ -14,6 +14,7 @@ module.exports = {
 
             ],
             electron: './src/electron/**/*.*',
+            cordova: './src/BoardZ/cordova.js',
             main: [
                 './src/BoardZ/index.html'
             ],
