551f94f4d4cd20c5630dd460d64eb8cb2ae28fc7
Thorsten Hans <thorsten.hans@gmail.com>
2016-02-25 21:58:36 +0100

    moved systemJS setup to it's own file

---
+++ src/BoardZ/system.setup.js
@@ -0,0 +1,9 @@
+System.config({
+    packages: {
+        app: {
+            format: 'register',
+            defaultExtension: 'js'
+        }
+    }
+});
+System.import('app/main').then(null, console.error.bind(console));
