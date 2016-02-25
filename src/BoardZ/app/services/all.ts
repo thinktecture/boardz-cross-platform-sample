ef62550b6413e5f926733a375a975e385d4dd3cb
Manuel Rauber <manuel.rauber@thinktecture.com>
2016-02-25 22:44:03 +0100

    More cleanup of services

--- src/BoardZ/app/services/all.ts
+++ src/BoardZ/app/services/all.ts
@@ -1,8 +1,7 @@
 import {provide} from 'angular2/core';
-import {ApplicationConfiguration, Configuration} from '../app-config';
+import {Configuration} from '../app-config';
 import {AuthenticatedHttp} from './authenticated.http';
 import {LoginService} from './login.service';
-import {LogLevel} from '../models/loglevel';
 import {GamesService} from './games.service';
 import {GeolocationService} from './geolocation.service';
 import {PlayersService} from './players.service';
@@ -12,14 +11,15 @@ import {UiNotificationService} from './ui.notification.service';
 import {CameraService} from './camera.service';
 import {DesktopCameraService} from './desktop.camera.service';
 import {MobileCameraService} from './mobile.camera.service';
+import {XHRBackend, ConnectionBackend} from 'angular2/http';
 
 declare var window;
 
 export var APP_SERVICES = [
+    provide(ConnectionBackend, { useClass: XHRBackend }),
     Configuration,
     AuthenticatedHttp,
     LoginService,
-    LogLevel,
     GamesService,
     GeolocationService,
     PlayersService,
