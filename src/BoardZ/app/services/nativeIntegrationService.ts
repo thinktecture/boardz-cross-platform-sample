85e33b7915662ebdfec04023fa78a7f6964ce84b
Manuel Rauber <manuel.rauber@thinktecture.com>
2016-04-19 21:34:36 +0200

    Added possible workaround for "back" navigation

--- src/BoardZ/app/services/nativeIntegrationService.ts
+++ src/BoardZ/app/services/nativeIntegrationService.ts
@@ -1,20 +1,35 @@
-import {Injectable} from 'angular2/core';
+import {Injectable, ApplicationRef} from 'angular2/core';
 import {Router} from 'angular2/router';
+import {PlatformInformationService} from './platformInformationService';
 
 declare var require;
 
 @Injectable()
 export class NativeIntegrationService {
-    constructor(private _router: Router) {
+    constructor(private _router: Router,
+                private _platformInformationService: PlatformInformationService,
+                private _applicationRef: ApplicationRef) {
     }
 
     public init() {
         var that = this;
 
-        if (window.navigator.userAgent.match(/Electron/) !== null) {
+        if (this._platformInformationService.isDesktop) {
             require('electron').ipcRenderer.on('navigateTo', function (event, data) {
                 that._router.navigate([data]);
             });
         }
+
+        this.applyBackWorkaround();
+    }
+
+    // https://github.com/angular/angular/issues/7722
+    // https://github.com/angular/angular/issues/7873
+    private applyBackWorkaround() {
+        this._router.subscribe(() => {
+            setTimeout(() => {
+                this._applicationRef.tick();
+            });
+        });
     }
 }
