ef6774fb3bb1d360d6eb75224b6fdc3fadfbda8f
Thorsten Hans <thorsten.hans@gmail.com>
2016-06-06 20:05:42 +0200

    removed old system cfg and introduced new cfg required by rc1

--- src/BoardZ/system.setup.js
+++ src/BoardZ/system.setup.js
@@ -1,11 +1,13 @@
-System.config({
-    packages: {
-        app: {
-            format: 'register',
-            defaultExtension: 'js'
-        }
-    },
-    map: {
+/**
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ */
+(function (global) {
+    // map tells the System loader where to look for things
+    var map = {
+        'app': 'app', // 'dist',
+        '@angular': '/@angular',
+        'rxjs': '/rxjs',
         'jquery': 'scripts/bundles/jquery-2.1.4.js',
         'bootstrap/js/bootstrap': 'scripts/bundles/bootstrap.js',
         'admin-lte/js/app': 'scripts/bundles/app.js',
@@ -18,8 +20,41 @@ System.config({
         'signalr/signalr': 'scripts/bundles/signalr.js',
         'leaflet/leaflet': 'scripts/bundles/leaflet-src.js',
         'fastclick/fastclick': 'scripts/bundles/fastclick.js'
+    };
+    // packages tells the System loader how to load when no filename and/or no extension
+    var packages = {
+        'app': { main: 'main.js', defaultExtension: 'js' },
+        'rxjs': { defaultExtension: 'js' }
+    };
+    var ngPackageNames = [
+        'common',
+        'compiler',
+        'core',
+        'http',
+        'platform-browser',
+        'platform-browser-dynamic',
+        'router-deprecated'
+    ];
+    // Individual files (~300 requests):
+    function packIndex(pkgName) {
+        packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
     }
-});
+
+    // Bundled (~40 requests):
+    function packUmd(pkgName) {
+        packages['@angular/' + pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
+    };
+    // Most environments should use UMD; some (Karma) need the individual index files
+    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+    // Add package entries for angular packages
+    ngPackageNames.forEach(setPackageConfig);
+    var config = {
+        map: map,
+        packages: packages
+    }
+    System.config(config);
+})(this);
 
 System.import('app/main')
     .then(null, console.error.bind(console));
+
