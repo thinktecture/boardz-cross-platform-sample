e7f9f25b34546637cc21e232ff7324a67db9e49d
Thorsten Hans <thorsten.hans@gmail.com>
2016-06-29 10:34:05 +0200

    valid systemjs config

--- src/BoardZ/system.setup.js
+++ src/BoardZ/system.setup.js
@@ -6,8 +6,8 @@
     // map tells the System loader where to look for things
     var map = {
         'app': 'app', // 'dist',
-        '@angular': '/@angular',
-        'rxjs': '/rxjs',
+        '@angular': '@angular',
+        'rxjs': 'rxjs',
         'jquery': 'scripts/bundles/jquery-2.1.4.js',
         'bootstrap/js/bootstrap': 'scripts/bundles/bootstrap.js',
         'admin-lte/js/app': 'scripts/bundles/app.js',
@@ -24,30 +24,16 @@
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
         'app': { main: 'main.js', defaultExtension: 'js' },
-        'rxjs': { defaultExtension: 'js' }
+        'rxjs': { defaultExtension: 'js' },
+        '@angular/common': { main: 'index.js', defaultExtension: 'js' },
+        '@angular/compiler': { main: 'index.js', defaultExtension: 'js' },
+        '@angular/core': { main: 'index.js', defaultExtension: 'js' },
+        '@angular/http': { main: 'index.js', defaultExtension: 'js' },
+        '@angular/platform-browser': { main: 'index.js', defaultExtension: 'js' },
+        '@angular/platform-browser-dynamic': { main: 'index.js', defaultExtension: 'js' },
+        '@angular/router-deprecated': { main: 'index.js', defaultExtension: 'js' },
     };
-    var ngPackageNames = [
-        'common',
-        'compiler',
-        'core',
-        'http',
-        'platform-browser',
-        'platform-browser-dynamic',
-        'router-deprecated'
-    ];
-    // Individual files (~300 requests):
-    function packIndex(pkgName) {
-        packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
-    }
 
-    // Bundled (~40 requests):
-    function packUmd(pkgName) {
-        packages['@angular/' + pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
-    };
-    // Most environments should use UMD; some (Karma) need the individual index files
-    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-    // Add package entries for angular packages
-    ngPackageNames.forEach(setPackageConfig);
     var config = {
         map: map,
         packages: packages
@@ -55,6 +41,31 @@
     System.config(config);
 })(this);
 
-System.import('app/main')
+function backupModule() {
+    return new Promise(function (resolve, reject) {
+        window.module = module;
+        module = undefined;
+        resolve(true);
+    });
+}
+
+function restoreModule() {
+    return new Promise(function (resolve, reject) {
+        module = window.module;
+        resolve(true);
+    });
+}
+
+backupModule()
+    .then(function () {
+        return System.import('jquery');
+
+    })
+    .then(function () {
+        return restoreModule();
+    })
+    .then(function () {
+        return System.import('app/main');
+    })
     .then(null, console.error.bind(console));
 
