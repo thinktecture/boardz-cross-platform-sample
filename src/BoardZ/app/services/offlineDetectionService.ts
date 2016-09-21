71690a9b18cfba89a5e7ee3dec26021bf0812f1e
Thorsten Hans <thorsten.hans@gmail.com>
2016-09-21 21:02:09 +0200

    import observable from right package path

--- src/BoardZ/app/services/offlineDetectionService.ts
+++ src/BoardZ/app/services/offlineDetectionService.ts
@@ -3,7 +3,7 @@ import {Http} from '@angular/http';
 import {ApiConfig} from '../apiConfig';
 import {ConnectionState} from '../models/connectionState';
 import {OfflineConfig} from '../offlineConfig';
-import {Observable} from '/rxjs/Observable';
+import {Observable} from 'rxjs/Observable'
 import 'rxjs/add/operator/timeout';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
