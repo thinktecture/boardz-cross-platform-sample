985ffe6a2aff4b57dd10a4706d27838e69ddca8e
Christian Liebel <christian.liebel@thinktecture.com>
2016-09-22 12:08:11 +0200

    Create tempate for workshop

--- src/BoardZ/app/services/gamesService.ts
+++ src/BoardZ/app/services/gamesService.ts
@@ -21,7 +21,7 @@ export class GamesService {
     }
 
     public getAll(): Observable<Game[]> {
-        return this._http.get('api/boardgames/list').map(response => (<Game[]>response.json()));
+        // TODO
     }
 
     public deepClone(game: Game): Game {
@@ -29,26 +29,22 @@ export class GamesService {
     }
 
     public getGameCount(): Observable<number> {
-        return this.getAll().map(games => games.length);
+        // TODO
     }
 
     public getById(id: string): Observable<Game> {
-        return this._http.get(`api/boardgames/single?id=${id}`)
-            .map(response => <Game>response.json());
+        // TODO
     }
 
     public addGame(game: Game): Observable<string> {
-        return this._http.post(`api/boardgames/add`, JSON.stringify(game), this.getRequestOptions())
-            .map(response => <string>response.json());
+        // TODO
     }
 
     public updateGame(game: Game): Observable<string> {
-        return this._http.put(`api/boardgames/update`, JSON.stringify(game), this.getRequestOptions())
-            .map(response => game.id);
+        // TODO
     }
 
     public deleteGame(id: string): Observable<string> {
-        return this._http.delete(`api/boardgames/remove?id=${id}`)
-            .map(response => <string>response.text());
+        // TODO
     }
 }
--- src/BoardZ/app/services/mobileCameraService.ts
+++ src/BoardZ/app/services/mobileCameraService.ts
@@ -7,35 +7,6 @@ declare let window;
 
 export class MobileCameraService implements ICameraService {
     public getPhoto(): Observable<string> {
-        return Observable.create((observer: Observer<string>) => {
-            let removeDomListener = () => {
-                document.removeEventListener('deviceready', onCordovaDeviceReady);
-            };
-
-            let onCordovaDeviceReady = () => {
-                const camera = window.navigator.camera;
-
-                let options = {
-                    quality: 50,
-                    destinationType: camera.DestinationType.DATA_URL,
-                    sourceType: camera.PictureSourceType.CAMERA,
-                    encodingType: camera.EncodingType.PNG,
-                    saveToPhotoAlbum: false,
-                    correctOrientation: true
-                };
-
-                camera.getPicture(imageData => {
-                    observer.next('data:image/png;base64,' + imageData);
-                    removeDomListener();
-                    observer.complete();
-                }, error => {
-                    observer.error(error);
-                    removeDomListener();
-                    observer.complete();
-                }, options);
-            };
-
-            document.addEventListener('deviceready', onCordovaDeviceReady);
-        });
+        // TODO
     }
 }
