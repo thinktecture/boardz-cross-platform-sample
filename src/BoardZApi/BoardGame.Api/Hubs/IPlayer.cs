d41809eeea6e2698272425f185a431ae907797fc
Thorsten Hans <thorsten.hans@gmail.com>
2017-09-03 21:12:40 +0200

    feat(api): Remove old API

--- src/BoardZApi/BoardGame.Api/Hubs/IPlayer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BoardGame.Api.Hubs
-{
-    public interface IPlayer
-    {
-        void SomeoneStartedPlaying(string name, string game);
-    }
-}
--- src/BoardZApi/BoardGame.Api/Models/Coordinate.ts
+++
@@ -1,9 +0,0 @@
-
-module BoardGame.Api.Models {
-    
-    export class Coordinate {
-        
-        public latitude: number = 0;
-        public longitude: number = 0;
-    }
-}
